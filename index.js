cbb43b5535fe4826ed0afc43e10b2c4da7d3cec3
St Michael Egenamba <michaelegenamba@gmail.com>
2026-04-22 22:53:38 -0700

    First attempt

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const weatherApi = "https://api.weather.gov/alerts/active?area=";
 
 // Your code here!
 const button = document.getElementById("fetch-alerts");
-button.addEventListener("click", () => {
+button.addEventListener("click", async () => {
   const handleInput = document.getElementById("state-input");
   const value = handleInput.value;
   const result = value.trim().toUpperCase();
+
   if (!result) {
     throw new Error("Please enter a state abbreviation");
   }
+  const data = await fetchWeatherAlerts(result);
+
+  displayAlerts(data);
 });
 
 const fetchWeatherAlerts = async (state) => {
@@ -20,7 +24,34 @@ const fetchWeatherAlerts = async (state) => {
       throw new Error("Failed");
     }
     const parse = await res.json();
+    return parse;
   } catch (e) {
     console.error("Error:", e);
+    return { features: [] };
   }
+};
+
+const displayAlerts = (data) => {
+  const alertsDiv = document.getElementById("alerts-display");
+  alertsDiv.innerHTML = "";
+  const features = data.features;
+  if (!features || features.length === 0) {
+    alertsDiv.innerHTML = "<p>No active alerts at this time.</p>";
+    return;
+  }
+  features.forEach((alert) => {
+    const info = alert.properties;
+
+    const alertCard = document.createElement("div");
+    alertCard.classList.add("alert-card");
+
+    alertCard.innerHTML = `
+    <h3>${info.headline}</h3>
+    <p><strong>Severity:</strong> ${info.severity}</p>
+    <p><strong>Area:</strong> ${info.areaDesc}</p>
+    <p>${info.description}</p>
+  `;
+
+    alertsDiv.appendChild(alertCard);
+  });
 };
