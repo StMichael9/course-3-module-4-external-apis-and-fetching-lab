0175cc237b3ea8cab2b2c6ba0f7fd055cb122f7d
St Michael Egenamba <michaelegenamba@gmail.com>
2026-04-22 21:55:27 -0700

    Need to working on parsing json next

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 // index.js
-const weatherApi = "https://api.weather.gov/alerts/active?area="
+const weatherApi = "https://api.weather.gov/alerts/active?area=";
 
 // Your code here!
+const button = document.getElementById("fetch-alerts");
+button.addEventListener("click", () => {
+  const handleInput = document.getElementById("state-input");
+  const value = handleInput.value;
+  const result = value.trim().toUpperCase();
+  if (!result) {
+    throw new Error("Please enter a state abbreviation");
+  }
+});
+
+const fetchWeatherAlerts = async (state) => {
+  try {
+    const url = weatherApi + state;
+    const res = await fetch(url);
+    if (res.ok) {
+      throw new Error("Failed");
+    }
+  } catch (e) {
+    console.error("Error:", e);
+  }
+};
